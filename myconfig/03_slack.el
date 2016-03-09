a895897944e232c0d64ed46ab9d90e76ac2e3dce
Mahesh M <maheshmukundan@gmail.com>
2016-03-09 19:32:34 +0530

    Add slack configuration

---
+++ myconfig/03_slack.el
@@ -0,0 +1,10 @@
+
+(use-package slack
+  :commands (slack-start)
+  :init
+  (setq slack-enable-emoji t) ;; if you want to enable emoji, default nil
+  (setq slack-client-id "2523866285.23055479350")
+  (setq slack-client-secret "22f57a73590f27daf6ee70acca8f17db"))
+;;(setq slack-token ""))
+(setq slack-user-name "mahesh-from-emacs")
+
